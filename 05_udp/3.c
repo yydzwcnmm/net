ccf694cbed02d60739f2440c7b98a316b6cc8025
yydzwcnmm <111477503+yydzwcnmm@users.noreply.github.com>
2022-09-08 20:02:39 +0800

    v2.0

--- 05_udp/3.c
+++ 05_udp/3.c
@@ -1,3 +1,7 @@
+
+//本地套接字 服务器
+
+
 #include <stdio.h>
 #include <fcntl.h>
 #include <stddef.h>
@@ -5,6 +9,7 @@
 #include <unistd.h>
 #include <sys/un.h>
 #include <arpa/inet.h>
+
 int main(int argc, char *argv[])
 {
 
@@ -12,14 +17,14 @@ int main(int argc, char *argv[])
     int lfd = socket(AF_UNIX,SOCK_STREAM,0);
     struct sockaddr_un myaddr;
     myaddr.sun_family = AF_UNIX;
-    strcpy(myaddr.sum_path,"sock.s");
+    strcpy(myaddr.sun_path,"sock.s");
     bind(lfd,(struct sockaddr *)&myaddr,sizeof(myaddr));
     listen(lfd,128);
 
     //ti qu
     struct sockaddr_un cliaddr;
     socklen_t lenc = sizeof(cliaddr);
-    int cfd = accpet(lfd,(struct sockaddr *)&cliaddr,&lenc);
+    int cfd = accept(lfd,(struct sockaddr *)&cliaddr,&lenc);
 
     printf("new cilent file = %s\n",cliaddr.sun_path);
     //read and writer
@@ -34,7 +39,7 @@ int main(int argc, char *argv[])
         }
         else{
             printf("%s\n",buf);
-            send(cfd,buf,n,0)
+            send(cfd,buf,n,0);
         }
         
     }
